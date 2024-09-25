a1b3733ec4ddab17ffa0a3d145c078891f37dfad
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-09-24 19:45:15 -0700

    Make PointF4 return a PointF4; new methods.

--- src/main/java/com/github/tommyettinger/gdcrux/PointF4.java
+++ src/main/java/com/github/tommyettinger/gdcrux/PointF4.java
@@ -1,11 +1,12 @@
 package com.github.tommyettinger.gdcrux;
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector4;
+import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.NumberUtils;
 import com.github.tommyettinger.crux.Point4;
+
+import java.util.Random;
 
 /**
  * The same as {@link Vector4}, just implementing {@link Point4} and {@link Json.Serializable}.
@@ -31,6 +32,19 @@ public class PointF4 extends Vector4 implements Point4<PointF4>, Json.Serializab
     public PointF4(Point4<? extends Point4<?>> p) {
         this(p.x(), p.y(), p.z(), p.w());
     }
+
+    public PointF4(float[] values) {
+        super(values);
+    }
+
+    public PointF4(Vector2 vector, float z, float w) {
+        super(vector, z, w);
+    }
+
+    public PointF4(Vector3 vector, float w) {
+        super(vector, w);
+    }
+
 
     /**
      * Returns true if this type of point uses {@code float} or {@code double} for its components, or false otherwise.
@@ -141,11 +155,187 @@ public class PointF4 extends Vector4 implements Point4<PointF4>, Json.Serializab
     }
 
     @Override
-    public Vector4 setToRandomDirection() {
-        return set((float) Distributor.normal(MathUtils.random.nextLong()),
-                (float) Distributor.normal(MathUtils.random.nextLong()),
-                (float) Distributor.normal(MathUtils.random.nextLong()),
-                (float) Distributor.normal(MathUtils.random.nextLong())).nor();
+    public PointF4 set(Vector4 vector) {
+        super.set(vector);
+        return this;
+    }
+
+    @Override
+    public PointF4 set(float[] values) {
+        super.set(values);
+        return this;
+    }
+
+    @Override
+    public PointF4 set(Vector2 vector, float z, float w) {
+        super.set(vector, z, w);
+        return this;
+    }
+
+    @Override
+    public PointF4 set(Vector3 vector, float w) {
+        super.set(vector, w);
+        return this;
+    }
+
+    @Override
+    public PointF4 add(Vector4 vector) {
+        super.add(vector);
+        return this;
+    }
+
+    @Override
+    public PointF4 add(float x, float y, float z, float w) {
+        super.add(x, y, z, w);
+        return this;
+    }
+
+    @Override
+    public PointF4 add(float values) {
+        super.add(values);
+        return this;
+    }
+
+    @Override
+    public PointF4 sub(Vector4 a_vec) {
+        super.sub(a_vec);
+        return this;
+    }
+
+    @Override
+    public PointF4 sub(float x, float y, float z, float w) {
+        super.sub(x, y, z, w);
+        return this;
+    }
+
+    @Override
+    public PointF4 sub(float value) {
+        super.sub(value);
+        return this;
+    }
+
+    @Override
+    public PointF4 scl(float scalar) {
+        super.scl(scalar);
+        return this;
+    }
+
+    @Override
+    public PointF4 scl(Vector4 other) {
+        super.scl(other);
+        return this;
+    }
+
+    @Override
+    public PointF4 scl(float vx, float vy, float vz, float vw) {
+        super.scl(vx, vy, vz, vw);
+        return this;
+    }
+
+    @Override
+    public PointF4 mulAdd(Vector4 vec, float scalar) {
+        super.mulAdd(vec, scalar);
+        return this;
+    }
+
+    @Override
+    public PointF4 mulAdd(Vector4 vec, Vector4 mulVec) {
+        super.mulAdd(vec, mulVec);
+        return this;
+    }
+
+    @Override
+    public PointF4 nor() {
+        super.nor();
+        return this;
+    }
+
+    @Override
+    public PointF4 lerp(Vector4 target, float alpha) {
+        super.lerp(target, alpha);
+        return this;
+    }
+
+    @Override
+    public PointF4 interpolate(Vector4 target, float alpha, Interpolation interpolator) {
+        super.interpolate(target, alpha, interpolator);
+        return this;
+    }
+
+    @Override
+    public PointF4 limit(float limit) {
+        super.limit(limit);
+        return this;
+    }
+
+    @Override
+    public PointF4 limit2(float limit2) {
+        super.limit2(limit2);
+        return this;
+    }
+
+    @Override
+    public PointF4 setLength(float len) {
+        super.setLength(len);
+        return this;
+    }
+
+    @Override
+    public PointF4 setLength2(float len2) {
+        super.setLength2(len2);
+        return this;
+    }
+
+    @Override
+    public PointF4 clamp(float min, float max) {
+        super.clamp(min, max);
+        return this;
+    }
+
+    /**
+     * For each component, this clamps it between min and max, inclusive.
+     * @param min the minimum value allowable for any component, inclusive
+     * @param max the maximum value allowable for any component, inclusive
+     * @return this, after modifications
+     */
+    public PointF4 clampEach (float min, float max) {
+        x = Math.min(Math.max(x, min), max);
+        y = Math.min(Math.max(y, min), max);
+        z = Math.min(Math.max(z, min), max);
+        w = Math.min(Math.max(w, min), max);
+        return this;
+    }
+
+    /**
+     * Sets each component so it only has a fractional value, by subtracting the floor from each component.
+     * This produces a non-negative float for each component, between 0.0 inclusive and 1.0 exclusive, unless a
+     * component is outside the safe range for {@link MathUtils#floor(float)} (-16384.0 at the lowest).
+     * <pre>
+     * The result of fract() for a component with a value of  1.25 will be 0.25 .
+     * The result of fract() for a component with a value of -1.25 will be 0.75 .
+     * </pre>
+     *
+     * @return this, after modifications
+     */
+    public PointF4 fract () {
+        x -= MathUtils.floor(x);
+        y -= MathUtils.floor(y);
+        z -= MathUtils.floor(z);
+        w -= MathUtils.floor(w);
+        return this;
+    }
+
+    @Override
+    public PointF4 setToRandomDirection() {
+        return setToRandomDirection(MathUtils.random);
+    }
+
+    public PointF4 setToRandomDirection(Random random) {
+        return set(
+                (float) Distributor.normal(random.nextLong()),
+                (float) Distributor.normal(random.nextLong()),
+                (float) Distributor.normal(random.nextLong()),
+                (float) Distributor.normal(random.nextLong())).nor();
     }
 
     @Override
